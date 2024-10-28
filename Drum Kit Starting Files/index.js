fb647cacb58a9c3b45843b4501cda9fa198efde9
Reetika Kushwaha <97019332+reetikakushwaha@users.noreply.github.com>
2024-10-28 10:45:45 +0530

    keypress enabled

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -39,6 +39,48 @@ for (var i = 0; i < numberOfDrumButtones; i++) {
         }
     });
 }
+// document.addEventListener('keypress', function(event){
+//     alert("key pressed")
+// })
+for (var i = 0; i < numberOfDrumButtones; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("keypress", function (event) {
+        
+        
+        switch(event.key){
+            case 'w':
+                var audio = new Audio('sounds/tom-1.mp3');
+                audio.play();
+              break;
+            case 'a':
+                var audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
+              break;
+            case 's':
+                var audio = new Audio('sounds/tom-3.mp3');
+                audio.play();
+              break;
+            case 'd':
+                var audio = new Audio('sounds/tom-4.mp3');
+                audio.play();
+              break;
+            case 'j':
+                var audio = new Audio('sounds/snare.mp3');
+                audio.play();
+              break;
+            case 'k':
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();
+              break;
+            case 'l':
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
+              break;
+            default:
+                console.log("invalid")
+        }
+    });
+}
 
 
 
